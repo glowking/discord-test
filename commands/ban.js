de241181406de060b50e90791f18da727da0f3e7
glowking <44907678+glowking@users.noreply.github.com>
2021-01-14 09:49:45 -0500

    ban update

--- commands/ban.js
+++ commands/ban.js
@@ -1,31 +1,64 @@
-
-
 module.exports = {
-  name: 'ban', 
+  name: 'ban',
   permissions: 'BAN_MEMBERS',
-  
-  execute (message, args) {
-	
-  
-  
-  if (args.length < 2) {
-		return message.reply('Please mention the user you want to ban and specify a ban reason.');
-	}
 
-	const user = getUserFromMention(args[0]);
-	if (!user) {
-		return message.reply('Please use a proper mention if you want to ban someone.');
-	}
+  run: async (bot, message, args) => {
 
-	const reason = args.slice(1).join(' ');
-async function f() {	
-  
-  try {
-		await message.guild.members.ban(user, { reason });
-	} catch (error) {
-		return message.channel.send(`Failed to ban **${user.tag}**: ${error}`);
-	}
-  }
-	return message.channel.send(`Successfully banned **${user.tag}** from the server!`);
-} 
+        if (!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You do not have permission to perform this command!")
+
+        const user1 = message.mentions.users.first();
+        var member = message.mentions.members.first()
+
+
+        if (member) {
+
+            const member = message.mentions.members.first()
+
+            let reason = args.slice(2).join(' '); // arguments should already be defined
+            var user = message.mentions.users.first();
+
+            member.ban({ reason: `${args.slice(2).join(' ')}` }).then(() => {
+                let uEmbed = new RichEmbed()
+                    .setTitle('**' + `Sucessfully Banned ${user1.tag}!` + '**')
+                    .setThumbnail('https://i.gyazo.com/8988806671312f358509cf0fd69341006.jpg')
+                    .setImage('https://media3.giphy.com/media/H99r2HtnYs492/giphy.gif?cid=ecf05e47db8ad81dd0dbb6b132bb551add0955f9b92ba021&rid=giphy.gif')
+                    .setColor(0x320b52)
+                    .setTimestamp()
+                    .setFooter('Requested by ' + message.author.tag, 'https://i.gyazo.com/8988806671312f358509cf0fd69341006.jpg')
+                message.channel.send(uEmbed);
+
+            }).catch(err => {
+                message.channel.send('I was unable to kick the member');
+                console.log(err);
+            });
+        } else {
+            let user = message.mentions.users.first(),
+                userID = user ? user.id : args[1]
+
+            if (isNaN(args[1])) return message.channel.send("You need to enter a vlaid @Member or UserID #");
+
+            if (args[1].length <= 17 || args[1].length >= 19) return message.channel.send("UserID # must be 18 Digits");
+
+            if (userID) {
+                let bannedIDs = require('./bannedIDs.json').ids || []
+
+                if (!bannedIDs.includes(userID)) bannedIDs.push(userID)
+
+                fs.writeFileSync('./bannedIDs.json', JSON.stringify({ ids: bannedIDs }))
+                let reason = args.slice(2).join(' ');
+                let uEmbed = new RichEmbed()
+                    .setTitle('**' + `UserID #${args[1]}\n Will be Banned on Return!` + '**')
+                    .setThumbnail('https://i.gyazo.com/8988806671312f358509cf0fd69341006.jpg')
+                    .setImage('https://i.imgur.com/6Sh8csf.gif')
+                    .setColor(0x320b52)
+                    .setTimestamp()
+                    .setFooter('Requested by ' + message.author.tag, 'https://i.gyazo.com/8988806671312f358509cf0fd69341006.jpg')
+                    message.channel.send(uEmbed);
+
+            } else {
+                message.channel.send('Error')
+            }
+        }
+    }
+
 }
